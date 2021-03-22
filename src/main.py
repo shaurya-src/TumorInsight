865e365d9b983b046076fd75522cf0bf25ad0245
Shaurya Choudhary <shaurya.src@gmail.com>
2021-03-22 17:51:15 +0530

    Include import modules

--- src/main.py
+++ src/main.py
@@ -1,4 +1,26 @@
-import os
+import sys
+import random
+import math
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import matplotlib
+matplotlib.use("TkAgg")
+
+from tkinter import Tk, ttk, messagebox
+from tkinter.filedialog import askopenfilename
+
+from PIL import Image
+
+from scipy import ndimage, misc
+from imageio import imread
+from skimage import data
+from skimage.morphology import watershed, disk
+from skimage.filters import rank
+from skimage.util import img_as_ubyte
+
+from func import *
 
 def main():
     print("Hello world")
